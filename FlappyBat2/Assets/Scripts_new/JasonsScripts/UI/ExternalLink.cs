f56535a7096ad4037ca035cad16da2053f2bc9ad
jzraiti <jzraiti@gmail.com>
2023-04-05 15:08:01 -0500

    Add donation scene

---
+++ FlappyBat2/Assets/Scripts_new/JasonsScripts/UI/ExternalLink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalLink : MonoBehaviour
+{
+    public void OpenPatreon()
+    {
+        Application.OpenURL("https://www.patreon.com/user?u=67700660&fan_landing=true&view_as=public");
+    }
+
+}
